d90d8c6a7f2c76af2de9db19ddb1556e6ef5bca3
kbaikov <konstantin.baikov@gmail.com>
2017-12-01 14:16:02 +0100

    Advent Day 1 solved

---
+++ advent2017_1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# http://adventofcode.com/2017
+
+
+def advent_sum(a):
+    matches = []
+    for i, item in enumerate(a[:-1]):
+        if item == a[i+1]:
+            matches.append(int(item))
+    if a[-1] == a[0]:
+        matches.append(int(a[-1]))
+    return(sum(matches))
+
+
+def advent_half_list_sum(a):
+    matches = []
+    half = len(a) // 2
+    for i, item in enumerate(a):
+        if i < half:
+            if item == a[half + i]:
+                matches.append(int(item))
+        else:
+            if item == a[i - half]:
+                matches.append(int(item))
+    # if a[-1] == a[0]:
+    #     matches.append(int(a[-1]))
+    return(sum(matches))
+
+
+if __name__ == '__main__':
+    with open('input_advent2017_1.txt') as file:
+        for line in file:
+            print(advent_sum((line.strip())))
+            print(advent_half_list_sum((line.strip())))
+    # print(advent_half_list_sum("1221"))
---
+++ test_advent2017_1.py
@@ -0,0 +1,24 @@
+import pytest
+
+from advent2017_1 import advent_sum, advent_half_list_sum
+
+
+@pytest.mark.parametrize("input, expected", [
+    ("1122", 3),
+    ("1111", 4),
+    ("1234", 0),
+    ("91212129", 9),
+])
+def test_advent_sum(input, expected):
+    assert advent_sum(input) == expected
+
+
+@pytest.mark.parametrize("input, expected", [
+    ("1212", 6),
+    ("1221", 0),
+    ("123425", 4),
+    ("123123", 12),
+    ("12131415", 4),
+])
+def test_advent_half_list_sum(input, expected):
+    assert advent_half_list_sum(input) == expected

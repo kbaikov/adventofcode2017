599bbbc22b962c612b23a81a54e2f0370c3ba545
kbaikov <konstantin.baikov@gmail.com>
2017-12-02 18:15:04 +0100

    Add day 2 tests

---
+++ test_advent2017_2.py
@@ -0,0 +1,21 @@
+import pytest
+
+from advent2017_2 import evenly_divided, difference
+
+
+@pytest.mark.parametrize("input, expected", [
+    ("5 1 9 5", 8),
+    ("7 5 3", 4),
+    ("2 4 6 8", 6),
+])
+def test_difference(input, expected):
+    assert difference(input) == expected
+
+
+@pytest.mark.parametrize("input, expected", [
+    ("5 9 2 8", 4),
+    ("9 4 7 3", 3),
+    ("3 8 6 5", 2),
+])
+def test_evenly_divided(input, expected):
+    assert evenly_divided(input) == expected

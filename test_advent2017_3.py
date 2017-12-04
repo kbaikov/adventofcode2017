e0d48e0afe4d995261227eedadbb70822b3343fb
kbaikov <konstantin.baikov@gmail.com>
2017-12-04 14:23:58 +0100

    Add tests for day 4 and 3

---
+++ test_advent2017_3.py
@@ -0,0 +1,13 @@
+import pytest
+
+from advent2017_3 import next_direction
+
+
+@pytest.mark.parametrize("input, expected", [
+    ("east", "north"),
+    ("north", "west"),
+    ("west", "south"),
+    ("south", "east"),
+])
+def test_next_direction(input, expected):
+    assert next_direction(input) == expected

ddbfac7a239f5909baa4a6e4d28b82955a66e1ae
kbaikov <konstantin.baikov@gmail.com>
2017-12-05 17:59:41 +0100

    Day 5 part 2

--- advent2017_5.py
+++ advent2017_5.py
@@ -25,11 +25,31 @@ def process(instructions):
             return step_number
 
 
+def process_3_or_more(instructions):
+    step_number = 0
+    current_index = 1
+    next_index = 1
+    current_value = instructions[current_index]
+    while True:
+        try:
+            step_number += 1
+            current_index = next_index
+            jump_value = instructions[current_index]
+            if jump_value >=3:
+                instructions[current_index] -= 1
+            else:
+                instructions[current_index] += 1
+            next_index = current_index + jump_value
+            current_value = instructions[next_index]
+        except (KeyError, IndexError) as e:
+            return step_number
+
+
 
 if __name__ == '__main__':
     instructions = dict()
     with open('input_advent2017_5.txt') as file:
         for index, line in enumerate(file, 1):
             instructions[index] = int(line.strip())
-    # print(process({1:0, 2:3, 3:0, 4:1, 5:-3}))
-    print(process(instructions))
+    print(process_3_or_more({1:0, 2:3, 3:0, 4:1, 5:-3}))
+    print(process_3_or_more(instructions))
--- test_advent2017_5.py
+++ test_advent2017_5.py
@@ -1,6 +1,6 @@
 import pytest
 
-from advent2017_5 import process
+from advent2017_5 import process, process_3_or_more
 
 
 @pytest.mark.parametrize("input, expected", [
@@ -10,3 +10,8 @@ def test_process(input, expected):
     assert process(input) == expected
 
 
+@pytest.mark.parametrize("input, expected", [
+    ({1: 0, 2: 3, 3: 0, 4: 1, 5: -3}, 10),
+])
+def test_process_3_or_more(input, expected):
+    assert process_3_or_more(input) == expected

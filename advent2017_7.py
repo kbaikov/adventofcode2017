6e7890b67190eb8b4e8ee813c2c33330f01284bb
kbaikov <konstantin.baikov@gmail.com>
2017-12-08 17:12:28 +0100

    Day 7 solved

--- advent2017_7.py
+++ advent2017_7.py
@@ -7,7 +7,7 @@ from functools import lru_cache
 from collections import Counter
 from pprint import pprint
 import copy
-from anytree import Node, RenderTree
+from anytree import Node, RenderTree, LevelOrderGroupIter, PreOrderIter, PostOrderIter
 import csv
 
 
@@ -16,62 +16,68 @@ def create_nodes(s):
         node, value, _, *children = s
         for i, child in enumerate(children):
             children[i] = child.replace(",", "")
-        globals()[node] = Node(node)
+        globals()[node] = Node(node, v=eval(value))
         node_list.append(node)
     except ValueError as e:
         node, value = s
-        globals()[node] = Node(node)
+        globals()[node] = Node(node, v=eval(value))
         node_list.append(node)
 
 
 def assign_children(s):
     try:
-        node, value, _, *children = s
-        for i, child in enumerate(children):
-            children[i] = child.replace(",", "")
-        node.children = children
-    except:
+        node, value, _, *ch = s
+        for i, child in enumerate(ch):
+            ch[i] = child.replace(",", "")
+        children_values = [eval(item) for item in ch]
+        globals()[node].children = children_values
+    except ValueError:
         pass
 
 
-def main(bank):
-    pass
+def siblings_sum(n):
+    result = dict()
+    for s in n.siblings:
+        result[s] = sum([v.v for v in s.descendants]) + s.v
+    result[n] = sum([v.v for v in n.descendants]) + n.v
+    return result
+
+
+def is_consistent(s):
+    return all(x == list(s.values())[0] for x in s.values())
+
+
+def difference(s):
+    l = list(s.values())
+    return max(l) - min(l)
 
 
 if __name__ == '__main__':
     node_list = []
     initial = dict()
-    with open('temp') as file:
+    with open('input_advent2017_7.txt') as file:
         reader = csv.reader(file, delimiter=' ')
         for row in reader:
             create_nodes(row)
+        file.seek(0)
+        for row in reader:
             assign_children(row)
-    print(node_list)
-    print(ugml.children)
-    print(RenderTree(ugml))
+    # print(node_list)
+    # ugml.children = [gyxo, ebii, jptl]
+    # tknk.children = [ugml, padx, fwft]
+    # setattr(ugml, "children", [gyxo, ebii, jptl])
+    # print(ugml.children)
+    tree_root = teuku.root
+    # print(RenderTree(tree_root))
+    print(tree_root)
+    # print(siblings_sum())
+    # print(is_consistent(siblings_sum(gyxo)))
+    # print([node.name for node in PreOrderIter(tree_root)])
+    for node in PreOrderIter(cwwwj):
+        if not is_consistent(siblings_sum(node)):
+            print(node.name, node.v, siblings_sum(node))
+            d = difference(siblings_sum(node))
+            print(node.v - d)
     # print(initial)
     # print(main(initial))
     # print(main({1:0, 2:2, 3:7, 4:0}))
-
-
-output = """
-
-                gyxo
-              /     
-         ugml - ebii
-       /      \     
-      |         jptl
-      |        
-      |         pbga
-     /        /
-tknk --- padx - havc
-     \        \
-      |         qoyq
-      |             
-      |         ktlj
-       \      /     
-         fwft - cntj
-              \     
-                xhth
-
-"""
--- test_advent2017_7.py
+++ test_advent2017_7.py
@@ -1,10 +1,11 @@
 import pytest
 
-from advent2017_7 import main
+from advent2017_7 import is_consistent
 
 
 @pytest.mark.parametrize("input, expected", [
-    ({1: 0, 2: 2, 3: 7, 4: 0}, {1: 2, 2: 4, 3: 1, 4: 2}),
+    ({1: 2, 2: 2, 3: 2, 4: 2}, True),
+    ({1: 2, 2: 2, 3: 3, 4: 2}, False),
 ])
-def test_redistribute(input, expected):
-    assert redistribute(input) == expected
+def test_is_consistent(input, expected):
+    assert is_consistent(input) == expected

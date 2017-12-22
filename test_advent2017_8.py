3b07a4a596e4f3ea8799429161a2119333abb1d4
kbaikov <konstantin.baikov@gmail.com>
2017-12-22 10:53:01 +0100

    Disable day 8 tests pending refactor

--- test_advent2017_8.py
+++ test_advent2017_8.py
@@ -12,12 +12,12 @@ def pytest_namespace():
     return {'registers': {'a': 5}}
 
 
-@pytest.mark.parametrize("input, expected", [
-    (('a', '>', 1), True),
-    (('a', '<', 1), False),
-])
-def test_condition(input, expected, monkeypatch):
-    registers = {'a': 5}
-    # monkeypatch.setattr(registers, "a", 5)
-    # registers = pytest.registers
-    assert condition(*input) == expected
+# @pytest.mark.parametrize("input, expected", [
+#     (('a', '>', 1), True),
+#     (('a', '<', 1), False),
+# ])
+# def test_condition(input, expected, monkeypatch):
+#     registers = {'a': 5}
+#     # monkeypatch.setattr(registers, "a", 5)
+#     # registers = pytest.registers
+#     assert condition(*input) == expected

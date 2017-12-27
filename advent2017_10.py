0d82d814736b4456446b553b35d8fd4c91dacc35
kbaikov <konstantin.baikov@gmail.com>
2017-12-27 03:20:30 +0100

    Day 10 solved

--- advent2017_10.py
+++ advent2017_10.py
@@ -2,12 +2,13 @@
 # http://adventofcode.com/2017
 
 import math
-from itertools import cycle, dropwhile, permutations
+from itertools import cycle, dropwhile, permutations, accumulate
 from functools import lru_cache
 from collections import Counter
 from pprint import pprint
 import copy
 import csv
+import operator
 import logging as log
 import os
 import sys
@@ -16,12 +17,73 @@ LOGLEVEL = os.environ.get('LOGLEVEL', 'WARNING').upper()
 log.basicConfig(level=LOGLEVEL)
 
 
-def clean_exclamation(s):
-    pass
+def insert_from(place, from_l, to_l):
+    for i, item in enumerate(from_l):
+        try:
+            to_l[i+place] = item
+        except IndexError:
+            to_l[i+place-len(to_l)] = item
+    return to_l
+
+
+def select_reverse(place, length, to_l):
+    if place + length < len(to_l):
+        return list(reversed(to_l[place:place+length]))
+    else:
+        p = length - len(to_l[place:])
+        return list(reversed(to_l[place:] + to_l[:p]))
+
+
+def one_round(length_sequence, current_position, skip_size, init_list):
+    for length in length_sequence:
+        result_list = insert_from(
+                                current_position,
+                                select_reverse(
+                                                current_position,
+                                                length,
+                                                init_list),
+                                init_list)
+        current_position += length + skip_size
+        if current_position >= len(result_list):
+            current_position = current_position % len(result_list)
+        skip_size += 1
+        if skip_size >= len(result_list):
+            skip_size = skip_size % len(result_list)
+    return result_list, current_position, skip_size
+
+
+def knot_hash(input_string):
+    mega_list = list(range(256))
+    current_position = 0
+    skip_size = 0
+    lengths = [ord(i) for i in input_string]
+    lengths.extend([17, 31, 73, 47, 23])
+    for i in range(64):
+        mega_list, current_position, skip_size = one_round(lengths, current_position, skip_size, mega_list)
+    chunks = [mega_list[x:x+16] for x in range(0, len(mega_list), 16)]
+    dense_hash = [list(accumulate(chunk, func=operator.xor))[-1] for chunk in chunks]
+    hash_string = "".join([format(x, '02x') for x in dense_hash])
+    return hash_string
+
+
+def part1():
+    mega_list = list(range(256))
+    with open('input_advent2017_10.txt') as file:
+        lengths = [int(x) for x in file.readline().strip().split(',')]
+        result_list, _, _ = one_round(lengths, 0, 0, mega_list)
+    print(result_list[0] * result_list[1])
+
+
+def part2():
+    with open('input_advent2017_10.txt') as file:
+        input_string = file.readline().strip()
+        
+    print(knot_hash(input_string))
+
 
 def main():
-    with open('input_advent2017_10.txt') as file:
-        # print(score_group(clean_garbage(clean_exclamation(file.readline()))))
+    part1()
+    part2()
 
 
 if __name__ == '__main__':
--- test_advent2017_10.py
+++ test_advent2017_10.py
@@ -1,32 +1,39 @@
 import pytest
 
-from advent2017_10 import main
+from advent2017_10 import insert_from, select_reverse, one_round, knot_hash
 
 
 @pytest.mark.parametrize("input, expected", [
-    ('aaa!ddd!!cc', 'aaaddcc'),
-    ('!!!!a', 'a'),
+    ((1, [1, 2, 3], [3, 4, 5, 6]), [3, 1, 2, 3]),
+    ((0, [1, 2, 3], [3, 4, 5, 6]), [1, 2, 3, 6]),
+    ((2, [1, 2, 3], [3, 4, 5, 6]), [3, 4, 1, 2]),
+    ((3, [1, 2, 3], [3, 4, 5, 6]), [2, 3, 5, 1]),
 ])
-def test_clean_exclamation(input, expected):
-    assert clean_exclamation(input) == expected
+def test_insert_from(input, expected):
+    assert insert_from(*input) == expected
 
 
 @pytest.mark.parametrize("input, expected", [
-    ('a<>a', 'aa'),
-    ('a<asdf>a', 'aa'),
-    ('a<<<<>a', 'aa'),
-    ('<>', ''),
+    ((1, 2, [3, 4, 5, 6]), [5, 4]),
+    ((0, 3, [3, 4, 5, 6]), [5, 4, 3]),
+    ((2, 3, [3, 4, 5, 6]), [3, 6, 5]),
+    ((3, 3, [3, 4, 5, 6]), [4, 3, 6]),
 ])
-def test_clean_garbage(input, expected):
-    assert clean_garbage(input) == expected
+def test_select_reverse(input, expected):
+    assert select_reverse(*input) == expected
 
 
 @pytest.mark.parametrize("input, expected", [
-    ('{}', 1),
-    ('{{{}}}', 6),
-    ('{{},{}}', 5),
-    ('{{{},{},{{}}}}', 16),
-    ('{{<ab>},{<ab>},{<ab>},{<ab>}}', 9),
+    ('', 'a2582a3a0e66e6e86e3812dcb672a272'),
+    ('AoC 2017', '33efeb34ea91902bb2f59c9920caa6cd'),
+    ('1,2,3', '3efbe78a8d82f29979031a4aa0b16a9d'),
+    ('1,2,4', '63960835bcdc130f0b66d7ff4f6a5a8e'),
 ])
-def test_score_group(input, expected):
-    assert score_group(input) == expected
+def test_knot_hash(input, expected):
+    assert knot_hash(input) == expected
+
+@pytest.mark.parametrize("input, expected", [
+    (([3, 4, 1, 5], 0, 0, [0, 1, 2, 3, 4]), ([3, 4, 2, 1, 0], 4, 4)),
+])
+def test_one_round(input, expected):
+    assert one_round(*input) == expected
